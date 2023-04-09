d440b95b7d09a7238e35215a481f54a6dd6321c1
23Mubasshir <23mubasshir@gmail.com>
2023-04-09 22:39:22 +0600

    Header Done

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Job Hunter</h1>
+            <nav>
+                <a>Statistics</a>
+                <a>Applied Jobs</a>
+                <a>Blog</a>
+            </nav>
+            <button>Start Applying</button>
+        </div>
+    );
+};
+
+export default Header;
