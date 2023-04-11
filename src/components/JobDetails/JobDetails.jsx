c1f7fae964b9c9e05b8b62c227b6191119d39b5c
23Mubasshir <23mubasshir@gmail.com>
2023-04-11 13:18:00 +0600

    Job Details Page Done

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,31 +1,66 @@
 import React, { useEffect, useState } from "react";
 import "./JobDetails.css";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faCircleDollarToSlot, faCalendar, faCalendarCheck, faPhone, faVoicemail, faMailBulk, faMailForward, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const JobDetails = () => {
   const JobDetailsId = useParams();
-  console.log(JobDetailsId);
+
+  const [job, setJob] = useState([]);
 
   useEffect(() => {
     fetch("../../../public/FeaturedJobs.json")
       .then((res) => res.json())
       .then((data) => {
-        const jobData = data.find(job => job.id == JobDetailsId.jobId
-          );
-        console.log(data);
-        console.log(jobData);
+        const jobData = data.find((job) => job.id == JobDetailsId.jobId);
+        setJob(jobData);
       });
   }, []);
+
+  const {
+    job_description,
+    job_responsibility,
+    educational_requirements,
+    experiences,
+    salary,
+    job_title,
+    location,
+    phone,
+    email
+  } = job;
 
   return (
     <div>
       <div className="Job-details-banner">
         <h1>Job Details</h1>
       </div>
-      <div className="full-job-details">
-        <div className="Job-details-text"></div>
 
-        <div className="Job-details-cart"></div>
+      <div className="full-job-details">
+
+        <div className="Job-details-text">
+          <p><span className="bold-title">Job Description : </span>{job_description}</p>
+          <p><span className="bold-title">Job Responsibility : </span>{job_responsibility}</p>
+          <p><span className="bold-title">Educational Requirements : </span>{educational_requirements}</p>
+          <p><span className="bold-title">Experiences : </span>
+          {experiences}</p>
+        </div>
+
+        <div className="Job-details-cart">
+          <div className="job-details-cart-color">
+          <h3>Job Details</h3>
+          <p><span className="bold-title"><FontAwesomeIcon icon={faCircleDollarToSlot} /> Salary: </span>{salary}</p>
+          <p><span className="bold-title"><FontAwesomeIcon icon={faCalendarCheck} /> Job Title : </span>{job_title}</p>
+          <h3>Contact Information</h3>
+          <p><span className="bold-title"><FontAwesomeIcon icon={faPhone} /> Phone : </span>{phone}</p>
+          <p><span className="bold-title"><FontAwesomeIcon icon={faEnvelope}/> Email : </span>{email}</p>
+          <p><span className="bold-title"><FontAwesomeIcon icon={faLocationDot} /> Address : </span>{location}</p>
+          </div>
+          <button className="primary-btn">
+            <Link to="/applied-jobs">Apply Now</Link>
+          </button>
+        </div>
+
       </div>
     </div>
   );
