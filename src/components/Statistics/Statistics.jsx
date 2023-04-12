9a2d96b27a812cdebb776f343b0daf42aabbed65
23Mubasshir <23mubasshir@gmail.com>
2023-04-12 16:07:24 +0600

    statistics page DONE

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,86 @@
 import React from "react";
+import './Statistics.css'
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Tooltip
+} from "recharts";
 
 const Statistics = () => {
+  const data = [
+    {
+      subject: "Assignmnt-1",
+      A: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-2",
+      A: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-3",
+      A: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-4",
+      A: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-5",
+      A: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-6",
+      A: 60,
+      B: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-7",
+      A: 60,
+      fullMark: 60
+    },
+    {
+      subject: "Assignmnt-8",
+      A: 56,
+      fullMark: 60
+    }
+  ];
   return (
-    <div>
+    <div className="Statistics">
       <div className="Job-details-banner">
         <h1>Statistics</h1>
       </div>
-      <h1>This is Statistics page</h1>
+      <h3>Marks of my previous Assignments:</h3>
+      <div className="chart1">
+      <RadarChart
+      cx={500}
+      cy={600}
+      outerRadius={300}
+      width={1000}
+      height={1000}
+      data={data}
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="subject" />
+      <PolarRadiusAxis />
+      <Tooltip/>
+      <Radar
+        name="Mubasshir"
+        dataKey="A"
+        stroke="#8884d8"
+        fill="#8884d8"
+        fillOpacity={0.6}
+      />
+    </RadarChart>
+      </div>
     </div>
   );
 };
